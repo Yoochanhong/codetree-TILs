c91806fbfc5e625c4d6e0be6c1122ff409d5c6ad
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-05-10 08:34:36 +0900

    Title: 겹치는 선분들, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240510/겹치는 선분들/overlapping-lines.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k, c = 0;
+    char b;
+    vector<pair<int, int>> l;
+    cin >> n >> k;
+    for (int i = 0, a; i < n; i++){
+        cin >> a >> b;
+        if (b == 'L'){
+            l.emplace_back(c - a, 1);
+            l.emplace_back(c, -1);
+        } else {
+            l.emplace_back(c, 1);
+            l.emplace_back(c + 1, -1);
+        }
+    }
+    sort(l.begin(), l.end());
+    int r=0,s=0;
+    for (auto [v, w] :l){
+        r += w;
+        if (r >= k) s++;
+    }
+    cout << s;
+    return 0;
+}
