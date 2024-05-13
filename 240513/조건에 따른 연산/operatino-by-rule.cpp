6a5a974dc2f9030c1281363266c6243430a91b87
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-05-13 18:52:02 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240513/조건에 따른 연산/operatino-by-rule.cpp
+++ 240513/조건에 따른 연산/operatino-by-rule.cpp
@@ -5,9 +5,9 @@ int main() {
     std::cin >> n;
     while(n < 1000){
         i++;
-        if (n % 2) n = n * 3 + 1;
+        if (n % 2 == 0) n = n * 3 + 1;
         else n = n * 2 + 2;
     }
-    std::cout << i - 1;
+    std::cout << i;
     return 0;
 }
