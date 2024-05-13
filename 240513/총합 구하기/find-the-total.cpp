4f5dd28b3a0d6d7d1948ff53ba5c35065243fe44
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-05-13 18:37:45 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240513/총합 구하기/find-the-total.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a, b, r = 0;
+    std::cin >> a >> b;
+    for (int i = a; i <= b; i++){
+        if (i % 6 == 0 && i % 8) r += i;
+    }
+    std::cout << r;
+    return 0;
+}
