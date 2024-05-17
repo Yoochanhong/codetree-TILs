55698dd98dd58081c1a8987596a668a1c89dfa13
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-05-17 13:48:12 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240517/미는 횟수/number-of-pushes.cpp
+++ 240517/미는 횟수/number-of-pushes.cpp
@@ -6,10 +6,16 @@ int main() {
     string a, b;
     cin >> a >> b;
     int c = 0;
-    while (a != b) {
+    string d = a;
+    while (d != b) {
+        if (c > a.length()) {
+            c = -1;
+            break;
+        }
         c++;
-        a.insert(0, 1, a.back());
-        a.pop_back();
+        d.clear();
+        for (int i = c; i < a.length(); i++) d += a[i];
+        for (int i = 0; i < c; i++) d += a[i];
     }
     cout << c;
     return 0;
