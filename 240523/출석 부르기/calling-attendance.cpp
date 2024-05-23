408357ecfea80cd6a9d773e934dfcc686d85174e
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-05-23 09:36:04 +0900

    Title: 출석 부르기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/출석 부르기/calling-attendance.cpp
+++ 240523/출석 부르기/calling-attendance.cpp
@@ -3,6 +3,6 @@
 int main() {
     int a;
     std::cin >> a;
-    std::cout << (a == 1 ? "John" : a == 2 ? "Tom" : "Paul");
+    std::cout << (a == 1 ? "John" : a == 2 ? "Tom" : a == 3 ? "Paul" : "Vacancy");
     return 0;
 }
