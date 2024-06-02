dca329f9a0f5b01f2d2c49aa21807e858c9d8703
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-02 10:52:06 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240602/윤년인가/is-leap-year.cpp
+++ 240602/윤년인가/is-leap-year.cpp
@@ -3,6 +3,6 @@
 int main() {
     int y;
     std::cin >> y;
-    std::cout << (y % 4 == 0 || !(y % 100 == 0 && y % 400) ? "true" : "false");
+    std::cout << (y % 4 == 0 && !(y % 100 == 0 && y % 400) ? "true" : "false");
     return 0;
 }
