fae5969aafcca19771529c4b6758d950b2fc5821
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-02 10:46:08 +0900

    Title: 숫자의 계절은, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240602/숫자의 계절은/season-of-num.cpp
+++ 240602/숫자의 계절은/season-of-num.cpp
@@ -4,8 +4,8 @@ int main() {
     int n;
     std::cin >> n;
     if (3 <= n && n <= 5) std::cout << "Spring";
-    if (6 <= n && n <= 8) std::cout << "Summer";
-    if (9 <= n && n <= 11) std::cout << "Fail";
+    else if (6 <= n && n <= 8) std::cout << "Summer";
+    else if (9 <= n && n <= 11) std::cout << "Fail";
     else std::cout << "Winter";
     return 0;
 }
