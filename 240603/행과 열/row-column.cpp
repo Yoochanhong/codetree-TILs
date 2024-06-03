359991bba41a26dd1924abe9e790617c3376bc39
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-03 09:34:45 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240603/행과 열/row-column.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b; j++) {
+            std::cout << i * j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
