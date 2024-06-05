9df626ee789f8a261f0c22c90158dfc287b71baa
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-05 21:53:16 +0900

    Title: 일직선 구구단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240605/일직선 구구단/one-line-multiple.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            std::cout << i << " * " << j << " = " << i * j << "\n";
+        }
+    }
+    return 0;
+}
