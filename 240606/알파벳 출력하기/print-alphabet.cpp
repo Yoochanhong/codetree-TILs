3741215e7cfb42d463861493a9a4a345e345facb
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-06 20:59:11 +0900

    Title: 알파벳 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240606/알파벳 출력하기/print-alphabet.cpp
+++ 240606/알파벳 출력하기/print-alphabet.cpp
@@ -6,7 +6,7 @@ int main() {
     for (int i = 0, k = 65; i < n; i++) {
         for (int j = 0; j <= i; j++) {
             std::printf("%c", k++);
-            if (k == 90) k = 65;
+            if (k == 91) k = 65;
         }
         std::cout << "\n";
     }
