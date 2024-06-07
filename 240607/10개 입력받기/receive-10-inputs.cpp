0958ab55e5190e1e035714e5e5b1593f1319015a
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-07 14:38:27 +0900

    Title: 10개 입력받기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/10개 입력받기/receive-10-inputs.cpp
+++ 240607/10개 입력받기/receive-10-inputs.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-    for (int i = 0, a, s = 0; i < 10; i++) {
+    for (int i = 0, a, s = 0; i < 11; i++) {
         std::cin >> a;
-        if (!a) {
+        if (!a || i == 10) {
             std::printf("%d %.1lf", s, (double)s / i);
+            break;
         } else s += a;
     }
     return 0;
