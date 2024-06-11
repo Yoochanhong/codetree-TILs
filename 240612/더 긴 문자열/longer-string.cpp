ef15904c091941c21898ed87c94cea4136eee9c7
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-12 08:28:02 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240612/더 긴 문자열/longer-string.cpp
+++ 240612/더 긴 문자열/longer-string.cpp
@@ -4,6 +4,7 @@ int main() {
     std::string a, b;
     std::cin >> a >> b;
     if (a.size() > b.size()) std::cout << a << " " << a.size();
-    else std::cout << b << " " << b.size();
+    else if (a.size() < b.size()) std::cout << b << " " << b.size();
+    else std::cout << "same";
     return 0;
 }
