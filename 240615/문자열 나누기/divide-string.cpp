429db3a15d9fbb6d1a987991a6ba771100c02839
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-15 22:47:49 +0900

    Title: 문자열 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240615/문자열 나누기/divide-string.cpp
+++ 240615/문자열 나누기/divide-string.cpp
@@ -8,7 +8,7 @@ int main() {
         std::cin >> a;
         b += a;
     }
-    for (int i = 0; i < b.size() / 4; i++) {
+    for (int i = 0; i <= b.size() / 4; i++) {
         for (int j = 0; j < 5 && b[i * 5 + j]; j++) {
             std::cout << b[i * 5 + j];
         }
