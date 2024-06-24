5bfe7199232d582434e18c0fa9c44614018d4c5e
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-24 21:53:53 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240624/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d, e;
+    std::cin >> a >> b >> c >> d >> e;
+    std::cout << (char)a << " " << (char)b << " " << (char)c << " " << (char)d << " " << (char)e;
+    return 0;
+}
