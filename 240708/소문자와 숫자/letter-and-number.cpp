8db8b04f66f100ed4cd5b3e59f3e3275aa57b0ca
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-07-08 19:50:53 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240708/소문자와 숫자/letter-and-number.cpp
+++ 240708/소문자와 숫자/letter-and-number.cpp
@@ -6,7 +6,7 @@ int main() {
     std::cin >> a;
     for (auto &i : a) {
         if (i >= 65 && i <= 90) i += 32;
-        if ((i >= 48 && i <= 58) || (i >= 97 && i <= 122)) std::cout << i;
+        if ((i >= 48 && i <= 57) || (i >= 97 && i <= 122)) std::cout << i;
     }
     return 0;
 }
